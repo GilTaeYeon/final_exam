e1f56d840c6e41f0f8c60c3bc093cdbaa7e43d01
Taeyeon Gil <kiltaeyeon@naver.com>
2021-12-08 16:43:32 +0900

    v2

--- index.py
+++ index.py
@@ -9,10 +9,7 @@ import time
 app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
 
-# PWM 인스턴스 p를 만들고  GPIO 18번을 PWM 핀으로 설정, 주파수  = 100Hz
-p = GPIO.PWM(18, 100) 
 
 #센서에 연결한 Trig와 Echo 핀의 핀 번호 설정 
 TRIG = 23
@@ -26,32 +23,6 @@ GPIO.setup(ECHO,GPIO.IN)
 GPIO.output(TRIG, False)
 print("Waiting for sensor to settle")
 time.sleep(2)
-dis_result = "0cm"
-
-def ultra():
-    try:
-         
-        global dis_result
-        GPIO.output(TRIG, True)   # Triger 핀에  펄스신호를 만들기 위해 1 출력
-        time.sleep(0.00001)       # 10µs 딜레이 
-        GPIO.output(TRIG, False)
-        
-        while GPIO.input(ECHO)==0:
-            start = time.time()	 # Echo 핀 상승 시간 
-        while GPIO.input(ECHO)==1:
-            stop= time.time()	 # Echo 핀 하강 시간 
-            
-        check_time = stop - start
-        distance = check_time * 34300 / 2
-        dis_result = distance+"cm"
-        print("Distance : %.1f cm" % distance)
-            
-        time.sleep(0.4)	# 0.4초 간격으로 센서 측정 
-                        
-            
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
     
 
 
@@ -64,7 +35,6 @@ def home():
 def ultra():
     try:
         while True:
-            global dis_result
 
             GPIO.output(TRIG, True)   # Triger 핀에  펄스신호를 만들기 위해 1 출력
             time.sleep(0.00001)       # 10µs 딜레이 
@@ -77,13 +47,13 @@ def ultra():
                 
             check_time = stop - start
             distance = check_time * 34300 / 2
-            dis_result = distance+"cm"
-            print("Distance : %.1f cm" % distance)
+            dis_result = str(distance)+"cm"
                 
             time.sleep(0.4)	# 0.4초 간격으로 센서 측정 
 
             return dis_result                         # 함수가 'ok'문자열을 반환함
-    except :
+    except Exception as e:
+        print(e)
         return "fail"
 
 
